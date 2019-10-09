9490d5349ac0233c3fba171b342feab863ab5da9
Mlandvo Dlamini <mlandvomesh@gmail.com>
2019-10-09 12:28:54 +0200

    final design

--- lib/user_interface/home.dart
+++ lib/user_interface/home.dart
@@ -120,7 +120,7 @@ class Home extends StatelessWidget {
                                       margin: EdgeInsets.only(left: 10, top: 5),
                                       child: CircleAvatar(
                                         radius: 30,
-                                        child: Image.network('https://pin.it/jrynfsjo5nljfa'),
+                                        child: Image.asset('assets/images/dp.png'),
                                       ),
                                     ),
                                     Container(
@@ -197,7 +197,7 @@ class Home extends StatelessWidget {
                                         margin: EdgeInsets.only(left: 10, top: 5),
                                         child: CircleAvatar(
                                           radius: 30,
-                                          child: Image.network('https://pin.it/jrynfsjo5nljfa'),
+                                          child: Image.asset('assets/images/dp.png'),
                                         ),
                                       ),
                                       Container(
@@ -274,7 +274,7 @@ class Home extends StatelessWidget {
                                         margin: EdgeInsets.only(left: 10, top: 5),
                                         child: CircleAvatar(
                                           radius: 30,
-                                          child: Image.network('https://pin.it/jrynfsjo5nljfa'),
+                                          child: Image.asset('assets/images/dp.png'),
                                         ),
                                       ),
                                       Container(
@@ -284,7 +284,7 @@ class Home extends StatelessWidget {
                                           style: TextStyle(
                                               color: Colors.lightBlue,
                                               fontSize: 20,
-                                              fontWeight: FontWeight.w500
+                                              fontWeight: FontWeight.w500,
                                           ),
                                         ),
                                       )
@@ -339,7 +339,147 @@ class Home extends StatelessWidget {
                     )
                   ),
                 ),
-              ],)
+              ],),
+              Container(
+                margin: EdgeInsets.only(top: 20, right: 20, left: 20),
+                child: Text(
+                    'Options',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 5, left: 20, right: 20, bottom: 5),
+                child: Divider(
+                  color: Colors.lightBlue,
+                  thickness: 1,
+                ),
+              ),
+              Container(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Icon(Icons.person_outline, size: 25,),
+                  ),
+                  trailing: OutlineButton(
+                      onPressed: () => debugPrint('pressed'),
+                    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                    child: Text(
+                      'Explore',
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                      'Mercenaries',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Hire a skilled mercenary to get the job done from our various list of choices.',
+                    style: TextStyle(
+                      fontSize: 15
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 5, left: 20, right: 20, bottom: 5),
+                child: Divider(
+                  color: Colors.lightBlue,
+                  thickness: 1,
+                ),
+              ),
+
+              Container(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Icon(Icons.directions_car, size: 25,),
+                  ),
+                  trailing: OutlineButton(
+                    onPressed: () => debugPrint('pressed'),
+                    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                    child: Text(
+                      'Explore',
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    'Getaway Drivers',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Hire a skilled getaway drivers to get the job done from our various list of choices.',
+                    style: TextStyle(
+                        fontSize: 15
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 5, left: 20, right: 20, bottom: 5),
+                child: Divider(
+                  color: Colors.lightBlue,
+                  thickness: 1,
+                ),
+              ),
+
+              Container(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Icon(Icons.location_city, size: 25,),
+                  ),
+                  trailing: OutlineButton(
+                    onPressed: () => debugPrint('pressed'),
+                    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                    child: Text(
+                      'Explore',
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    'Safe Houses',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Rent a safe house or two to lay low from our various list of choices.',
+                    style: TextStyle(
+                        fontSize: 15
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 5, left: 20, right: 20, bottom: 5),
+                child: Divider(
+                  color: Colors.lightBlue,
+                  thickness: 1,
+                ),
+              ),
             ],
           )
         ],
